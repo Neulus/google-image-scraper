7efa3552862230fe1b6be62012ac64e1f8cbe24f
Alpha-1004 <66864997+Alpha-1004@users.noreply.github.com>
2021-11-26 21:40:30 +0900

    chore: updated major version (breaking changes)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='google-image-scraper',
     author='Alpha-1004',
     url='https://github.com/Alpha-1004/google-image-scraper',
-    version='1.0.0',
+    version='2.0.0',
     packages=['imagescraper'],
     license='MIT',
     description='Google image scraper is a tool to scrape images from google.',
